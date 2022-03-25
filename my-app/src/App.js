dad0a30e1625079b65531c500d54d8a79075bec2
Parul Gupta <dguptarajat@gmail.com>
2022-03-26 04:58:58 +0530

    refactor
    added navbar and footer component in App.js so that they don't have to re-render with every page.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,13 @@ import "./App.css";
 import {Home} from './frontend/pages/home/Home';
 import Mockman from 'mockman-js';
 import {Route, Routes} from 'react-router-dom';
+import {Navbar} from './frontend/components/navbar/Navbar'
+import {Footer} from './frontend/components/footer/Footer'
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         {/* <Route path="/cart" element={<Cart/>}/>
@@ -14,6 +17,7 @@ function App() {
         <Route path="/signup" element={<Signup/>}/> */}
         <Route path="/test-api" element={<Mockman/>}/>
       </Routes>
+      <Footer/>
     </div>
   );
 }
--- my-app/src/frontend/pages/home/Home.js
+++ my-app/src/frontend/pages/home/Home.js
@@ -1,19 +1,13 @@
 import './home.css';
-import {Navbar} from '../.././components/navbar/Navbar'
 import {Products} from '../.././components/products/Products'
 import {Filter} from '../.././components/filter/Filter'
-import {Footer} from '../.././components/footer/Footer'
 
 
 function Home() {
   return (
-    <div>
-      <Navbar/>
-        <div className='sidebar-and-products'>
+    <div className='sidebar-and-products'>
           <Filter/>
           <Products/>
-        </div>
-        <Footer/>
     </div>
   )
 }
