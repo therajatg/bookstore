0c2930a680f889525c77f6c246829bd7d9e20055
Parul Gupta <dguptarajat@gmail.com>
2022-03-28 17:27:03 +0530

    feature-got all the filters working using useReducer and useContext

--- src/frontend/components/products/Products.js
+++ src/frontend/components/products/Products.js
@@ -1,50 +1,41 @@
-import {useEffect, useState} from 'react';
-import axios from 'axios';
-import './products.css'
-import { useFilter } from '../../contexts/filterContext';
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./products.css";
+import { useFilter } from "../../contexts/filterContext";
 
 export function Products() {
-
-  const productListing = useFilter();
-  console.log(productListing)
+  const { finalProductList } = useFilter();
 
   return (
-    <div className='product-grid'>
-      {productListing.map(item => <img src={item.img}/>)}
+    <div className="product-grid">
+      {finalProductList.map(
+        ({ img, title, author, rating, fastDelivery, price }) => (
+          <div className="card-container">
+            <img className="img-dimension" src={img} />
+            <h3 className="margin-zero">{title}</h3>
+            <h6 className="margin-one">by {author}</h6>
+            <p className="rating margin-one">Rating: {rating}</p>
+            <h4 className="margin-one">
+              {fastDelivery
+                ? "Superfast delivery (within 24 hours)"
+                : "Standard Delivery"}
+            </h4>
+            <span className="margin-one flex-space-between">
+              <span className="font-price">&#8377;{price}</span>
+              <del className="margin-one gray-text">&#8377;{price + 368}</del>
+              <span className="off font-size-xs">
+                {Math.round((368 * 100) / price)}%off
+              </span>
+            </span>
+            <a
+              href="#"
+              className="button-contained add-to-cart-button margin-one"
+            >
+              Add To Cart
+            </a>
+          </div>
+        )
+      )}
     </div>
-  )
-} 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// {    productListing.map(({img, title, author, rating, price}) => <div className="card-container">
-//        <img className='img-dimension' src={img} />
-//        <h3 className='margin-zero'>{title}</h3>
-//        <h6 className='margin-one'>by {author}</h6>
-//        <p className="rating margin-one">Rating: {rating}</p>
-//        <h4 className='margin-one'>Superfast delivery (within 24 hours)</h4>
-//        <span className='margin-one flex-space-between'>
-//          <span className='font-price'>&#8377;{price}</span>
-//          <del className='margin-one gray-text'>&#8377;{price+368}</del>
-//          <span className='off font-size-xs'>{Math.round(368*100/price)}%off</span>
-//         </span>
-//        <a href="#" className="button-contained add-to-cart-button margin-one">Add To Cart</a>
-//        </div>
-//     )}
-
-
+  );
+}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 import { FilterProvider } from "./frontend/contexts/filterContext";
 
 // Call make Server
@@ -12,9 +12,9 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <FilterProvider>
-    <App />
-    </FilterProvider>
+      <FilterProvider>
+        <App />
+      </FilterProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
