2eb2f757d2b03dbc7481cdf69b670616df5aa486
Parul Gupta <dguptarajat@gmail.com>
2022-03-31 12:33:21 +0530

    products updation

--- src/frontend/components/products/Products.js
+++ src/frontend/components/products/Products.js
@@ -1,10 +1,34 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
 import styles from "./products.module.css";
 import { useFilter } from "../../contexts/filterContext";
+import { useCart } from "../../contexts/cartContext";
+import { useEffect } from "react";
+import axios from "axios";
 
 export function Products() {
+  const encodedToken = localStorage.getItem("token");
   const { finalProductList } = useFilter();
+  const { setCartItems, cartItems } = useCart();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.post("/api/user/cart", {
+          headers: { authorization: encodedToken },
+        });
+        setCartItems(response.data.cart);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [cartItems]);
+
+  function cartItemHandler(_id) {
+    setCartItems((cartItems) => [
+      ...cartItems,
+      ...finalProductList.filter((item) => item._id === _id),
+    ]);
+    console.log(cartItems);
+  }
 
   return (
     <div className={styles.productGrid}>
@@ -27,12 +51,12 @@ export function Products() {
                 {Math.round((368 * 100) / price)}%off
               </span>
             </span>
-            <a
-              href="#"
-              className="button-contained add-to-cart-button margin-one"
+            <button
+              className="button-contained add-to-cart-button margin-one font-size-s"
+              onClick={(_id) => cartItemHandler(_id)}
             >
               Add To Cart
-            </a>
+            </button>
           </div>
         )
       )}
--- src/frontend/pages/cart/cart.js
+++ src/frontend/pages/cart/cart.js
@@ -1,15 +1,17 @@
 import { useFilter } from "../../contexts/filterContext";
+import { useCart } from "../../contexts/cartContext";
 import styles from "../../components/products/products.module.css";
 import sty from "./cart.module.css";
 
 export function Cart() {
   const { finalProductList } = useFilter();
+  const { cartItems } = useCart();
 
   return (
     <div className={sty.cartAndPriceDetails}>
       <div>
         <div className={`${styles.productGrid} ${sty.productGrid}`}>
-          {finalProductList.map(
+          {cartItems.map(
             ({ img, title, author, rating, fastDelivery, price }) => (
               <div className={`${styles.cardContainer} card-container`}>
                 <img className={styles.imgDimension} src={img} />
